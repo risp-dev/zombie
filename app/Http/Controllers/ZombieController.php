080b4e3e3f81d81f124593d2f616bba89a88b21c
Rimas <119005269+risp-dev@users.noreply.github.com>
2024-02-19 21:20:50 +0200

    Create controller and fetch The Zombies

--- app/Http/Controllers/ZombieController.php
+++ app/Http/Controllers/ZombieController.php
@@ -2,9 +2,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Zombie;
 
 class ZombieController extends Controller {
-    public function index() {
-        echo "Welcome to the Zombie Page!";
+    // public function index() {
+    //     echo "Welcome to the Zombie Page!";
+    public function show() {
+        $zombies = Zombie::all();
     }
 }
